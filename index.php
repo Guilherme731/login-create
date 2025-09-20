428e4e04b92351da1f4a6a79efc0e02b81f413e6
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-09-19 22:31:57 -0300

    feat: logica de login

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,11 +12,37 @@
 <body>
     <div class="container">
         <div class="box form-box">
+            <?php
+            
+                include("php/config.php");
+                if(isset($_POST['submit'])){
+                    $email = mysqli_real_escape_string($conn, $_POST['email']);
+                    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+                    $result = mysqli_query($conn, "SELECT * FROM users WHERE Email='$email' AND Password='$password'") or die('Erro.');
+                    $row = mysqli_fetch_assoc($result);
+
+                    if(is_array($row) && !empty($row)){
+                        $_SESSION['valid'] = $row['Email'];
+                        $_SESSION['username'] = $row['Username'];
+                        $_SESSION['age'] = $row['Age'];
+                        $_SESSION['id'] = $row['Id'];
+                    }else{
+                        echo "<div class='message'> <p>Senha ou usuário incorretos</p></div><br>";
+                        echo "<a href='index.php'><button class='btn'>Voltar</button>";
+                    }
+
+                    if(isset($_SESSION['valid'])){
+                        header("Location: home.php");
+                    }
+                }else{
+
+            ?>
             <header>Login</header>
             <form action="" method="POST">
                 <div class="field input">
-                    <label for="username">Username</label>
-                    <input type="text" name="username" id="username" required>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" required>
                 </div>
                 <div class="field input">
                     <label for="password">Password</label>
@@ -26,6 +56,9 @@
                 </div>
             </form>
         </div>
+        <?php
+                }
+        ?>
     </div>
 </body>
 </html>
