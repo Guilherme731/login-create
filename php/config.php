f846a038b6ea9c07795f5233fa65424b9a5745ca
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-09-19 22:15:31 -0300

    feat: conexao com db e logica do registro de usuario

---
+++ php/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "root", "sistema_login") or die("Não foi possível conectar.")
+
+
+?>
--- register.php
+++ register.php
@@ -8,10 +8,37 @@
 <body>
     <div class="container">
         <div class="box form-box">
+
+            <?php
+            
+                include("php/config.php");
+                if(isset($_POST['submit'])){
+                    $username = $_POST['username'];
+                    $email = $_POST['email'];
+                    $age = $_POST['age'];
+                    $password = $_POST['password'];
+
+                    $verify_query = mysqli_query($conn, "SELECT Email FROM users WHERE Email='$email'");
+
+                    if(mysqli_num_rows($verify_query) != 0){
+                        echo "<div class='message'> <p>Esse e-mail já está em uso, tente outro e-mail</p></div><br>";
+                        echo "<a href='javascript:self.history.back()'><button class='btn'>Voltar</button>";
+                    }
+                    else{
+                        mysqli_query($conn, "INSERT INTO users(Username, Email, Age, Password) VALUES('$username','$email','$age','$password')") or die("Ocorreu um erro.");
+                        echo "<div class='message'> <p>Nova conta criada com sucesso!</p></div><br>";
+                        echo "<a href='index.php'><button class='btn'>Fazer login</button>";
+                    }
+                }else{
+
+                
+            
+            ?>
+
             <header>Registrar-se</header>
             <form action="" method="POST">
                 <div class="field input">
-                    <label for="username">Username</label>
+                    <label for="username">Nome de Usuário</label>
                     <input type="text" name="username" id="username" autocomplete="off" required>
                 </div>
 
@@ -21,11 +48,11 @@
                 </div>
 
                 <div class="field input">
-                    <label for="age">Age</label>
+                    <label for="age">Idade</label>
                     <input type="number" name="age" id="age" autocomplete="off" required>
                 </div>
                 <div class="field input">
-                    <label for="password">Password</label>
+                    <label for="password">Senha</label>
                     <input type="password" name="password" id="password" autocomplete="off" required>
                 </div>
 
@@ -38,6 +65,9 @@
                 </div>
             </form>
         </div>
+        <?php
+            }
+        ?>
     </div>
 </body>
 </html>
